f1866032abbc07398aa2fb5404371aa6014856bb
menghan.wang <menghan.wang@thoughtworks.com>
2022-09-15 16:34:26 +0800

    test for complex instruction and multiple rovers

--- src/test/java/RoverSystemTest.java
+++ src/test/java/RoverSystemTest.java
@@ -4,20 +4,14 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 /*
-1. return the start position when given no instruction
-2. return the correct final position when turn right
-3. return the correct final position when turn left
-4. return the correct final position when move forward
-5. return the correct final position when given complex instruction
-6. return correct final positions for multiple rovers with different instructions
 7. return edge position when go beyond the coordinate
  */
 
 class RoverSystemTest {
 
+    RoverSystem roverSystem = new RoverSystem(5,5);
     @Nested
     class withinTheCoordinate{
-        RoverSystem roverSystem = new RoverSystem(5,5);
         @Test
         void given_1_1_N_should_return_the_start_point_when_given_no_instruction() {
             Rover rover = new Rover(1,1, Position.N);
@@ -66,6 +60,29 @@ class RoverSystemTest {
             roverSystem.processInstruction(rover,instruction);
 
             assertEquals("2 1 E \n",roverSystem.generateOutput().toString());
+        }
+
+        @Test
+        void given_1_1_N_should_return_1_2_N_when_process_LMLMLMLMM_as_instruction() {
+            Rover rover = new Rover(1,1, Position.N);
+            String instruction = "LMLMLMLMM";
+
+            roverSystem.processInstruction(rover,instruction);
+
+            assertEquals("1 2 N \n",roverSystem.generateOutput().toString());
+        }
+
+        @Test
+        void given_1_1_N_and_3_3_E_should_return_1_2_N_and_5_1_E_when_process_LMLMLMLMM_and_MMRMMRMRRM_as_instructions() {
+            Rover rover1 = new Rover(1,1, Position.N);
+            Rover rover2 = new Rover(3,3, Position.E);
+            String instruction1 = "LMLMLMLMM";
+            String instruction2 = "MMRMMRMRRM";
+
+            roverSystem.processInstruction(rover1,instruction1);
+            roverSystem.processInstruction(rover2,instruction2);
+
+            assertEquals("1 2 N \n5 1 E \n",roverSystem.generateOutput().toString());
         }
     }
 
